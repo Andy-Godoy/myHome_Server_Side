e1c7952b52870d5aba0df8cb096b3a1cb429cd44
andres.godoy <andres.godoyparise@gmail.com>
2023-09-01 15:30:51 -0300

    feat(controllers): se agregaron los controladores

---
+++ src/main/java/com/ad/myhome/controller/AgencyController.java
@@ -0,0 +1,19 @@
+package com.ad.myhome.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/agencies")
+public class AgencyController {
+
+    private final String CLASS_NAME = this.getClass().getSimpleName();
+
+    @GetMapping(value = "/test")
+    public String test(){
+        String msg = CLASS_NAME.concat(" esta funcionando ok!");
+        return msg;
+    }
+
+}
---
+++ src/main/java/com/ad/myhome/controller/PropertyController.java
@@ -0,0 +1,18 @@
+package com.ad.myhome.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/properties")
+public class PropertyController {
+
+    private final String CLASS_NAME = this.getClass().getSimpleName();
+
+    @GetMapping(value = "/test")
+    public String test(){
+        String msg = CLASS_NAME.concat(" esta funcionando ok!");
+        return msg;
+    }
+}
